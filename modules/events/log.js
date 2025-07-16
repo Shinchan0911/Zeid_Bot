350aa73d5e23c2db3387948dc4435cf7fbd4a93d
Shinchan0911 <98627396+Shinchan0911@users.noreply.github.com>
2025-07-16 16:52:54 +0700

    Update log.js

--- modules/events/log.js
+++ modules/events/log.js
@@ -1,32 +1,60 @@
+const chalk = require("chalk");
+const moment = require("moment-timezone");
+
 module.exports.config = {
     event_type: ["message"],
     name: "log",
-    version: "1.0.0",
+    version: "1.0.1",
     author: "ShinTHL09",
     description: "Log tin nhắn lên console",
     dependencies: {
-        "moment-timezone": ""
+        "moment-timezone": "",
+        "chalk": ""
     }
 };
 
 module.exports.run = async function({ api, event }) {
-    const moment = require("moment-timezone");
-    const logger = require("../../utils/logger");
-
+    const { threadId, data } = event;
     const time = moment.tz("Asia/Ho_Chi_Minh").format("D/MM/YYYY HH:mm:ss");
 
-    const groupInfo = await api.getGroupInfo(event.threadId);
-    const groupName = groupInfo.gridInfoMap[event.threadId].name ||"Tên không tồn tại";
-    const senderName = event.data.dName;
-    const content = event.data.content;
-    
-    var message = `
+    const senderName = data.dName || "Không rõ";
+    let content;
+
+    if (typeof data.content === "string") {
+        content = data.content;
+    } else if (typeof data.content?.title === "string") {
+        content = data.content.title;
+    } else {
+        content = "[Không phải tin nhắn văn bản]";
+    }
+
+    let message;
+
+    if (threadId === data.uidFrom) {
+        message = chalk.yellow(`
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+ Tin nhắn riêng từ: ${senderName}
+ Người dùng: ${senderName}
+ Nội dung: ${content}
+ Thời gian: ${time}
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━┛`);
+    } else {
+        let groupName;
+        try {
+            const groupInfo = await api.getGroupInfo(threadId);
+            groupName = groupInfo?.gridInfoMap?.[threadId]?.name || "Tên nhóm không xác định";
+        } catch {
+            groupName = "Không lấy được tên nhóm";
+        }
+
+        message = chalk.green(`
 ┏━━━━━━━━━━━━━━━━━━━━━━━━━━┓
  Nhóm: ${groupName}
  Người dùng: ${senderName}
- Nội dung: ${content}\n
+ Nội dung: ${content}
  Thời gian: ${time}
-┗━━━━━━━━━━━━━━━━━━━━━━━━━━┛
-`
-    return logger.log(message);
-}
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━┛`);
+    }
+
+    console.log(message);
+};
